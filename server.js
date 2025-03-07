a130ccd9e3c04f46e0eec906cead9539936b1987
Teeraphat Saengkum <kteeraphatt@gmail.com>
2025-03-07 08:48:19 +0700

    update frontend url

--- server.js
+++ server.js
@@ -6,26 +6,26 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 const cors = require("cors");
-const FRONTEND_URL =
+const frontendURL =
   process.env.NODE_ENV === "production"
     ? "https://www.siam10winery.com"
     : "http://127.0.0.1:5500";
 
-app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: frontendURL, credentials: true }));
 
-const secreatKey = 'pompatcharin';
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("✅ Connected to MongoDB"))
-  .catch(err => console.error("❌ MongoDB Connection Error:", err));
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
-const ProductSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  quantity: Number,
-});
+// const ProductSchema = new mongoose.Schema({
+//   name: String,
+//   price: Number,
+//   quantity: Number,
+// });
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -34,29 +34,32 @@ const UserSchema = new mongoose.Schema({
   address: String,
   phone: String,
   age: Number,
-  gender: String
+  gender: String,
 });
 
-const Product = mongoose.model("Product", ProductSchema);
+// const Product = mongoose.model("Product", ProductSchema);
 const User = mongoose.model("User", UserSchema);
-
-app.get('/', async (req, res) => {
-  res.json('hello world')
-})
 
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-    if (!user) return res.status(400).json({ error: "Invalid email or password" });
+    if (!user)
+      return res.status(400).json({ error: "Invalid email or password" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ error: "Invalid email or password" });
+    if (!isMatch)
+      return res.status(400).json({ error: "Invalid email or password" });
 
-    const token = jwt.sign({ userId: user._id }, secreatKey, { expiresIn: "1h" });
+    const token = jwt.sign({ userId: user._id }, process.env.SECREAT_KEY, {
+      expiresIn: "1h",
+    });
 
-    res.json({ token, user: { id: user._id, name: user.name, email: user.email } });
+    res.json({
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
   } catch (error) {
     res.status(500).json({ error: "Error logging in" });
   }
@@ -67,7 +70,8 @@ app.post("/register", async (req, res) => {
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ error: "Email already exists" });
+    if (existingUser)
+      return res.status(400).json({ error: "Email already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ ...req.body, password: hashedPassword });
@@ -79,24 +83,24 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/add-product", async (req, res) => {
-  try {
-    const product = new Product(req.body);
-    await product.save();
-    res.status(201).json({ message: "Product added", product });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// app.post("/add-product", async (req, res) => {
+//   try {
+//     const product = new Product(req.body);
+//     await product.save();
+//     res.status(201).json({ message: "Product added", product });
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
-app.get("/products", async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+// app.get("/products", async (req, res) => {
+//   try {
+//     const products = await Product.find();
+//     res.status(200).json(products);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
